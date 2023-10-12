994b8b6b56d8a8c6e9d2ee9edcd3bae540acc101
dragonmux <git@dragonmux.network>
2023-10-12 21:42:38 +0100

    build: Fixed the enable logic for backtraces so it works with any new enough compiler

--- build.rs
+++ build.rs
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 // SPDX-FileCopyrightText: 2022-2023 1BitSquared <info@1bitsquared.com>
 // SPDX-FileContributor: Written by Mikaela Szekely <mikaela.szekely@qyriad.me>
+// SPDX-FileContributor: Modified by Rachel Mant <git@dragonmux.network>
 //! To whomever might be reading this, understandably skeptical of build scripts:
 //! This build script is *optional*, and exists only to set *default* options under
 //! circumstances under which they are supported. Actually, all this build script does
 //! is detect if we are running nightly Rust, and enable backtrace support for errors
 //! if we are.
 
-use rustc_version::{version_meta, Channel};
+use std::str::FromStr;
+
+use rustc_version::{version_meta, Version};
 
 fn main()
 {
@@ -19,7 +22,7 @@ fn main()
     if let Some(_val) = std::env::var_os("CARGO_FEATURE_DETECT_BACKTRACE") {
         match version_meta() {
             Ok(version_meta) => {
-                if version_meta.channel == Channel::Nightly {
+                if version_meta.semver >= Version::from_str("1.65.0").unwrap() {
                     // Tell Cargo to enable backtraces.
                     println!("cargo:rustc-cfg=feature=\"backtrace\"");
                 }
